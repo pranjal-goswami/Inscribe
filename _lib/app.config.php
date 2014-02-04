135aa553d9e560476de92cc63e579855894a2a4a
Pranjal Goswami <pranjal.b.goswami@gmail.com>
2014-02-04 23:06:56 +0530

    Fixed all bugs

--- _lib/app.config.php
+++ _lib/app.config.php
@@ -9,10 +9,27 @@ define('S_ADMIN_FAIL','admin_login_failed');
 define('S_OWNER','owner');
 /* Database Connection Credentials */
 if(SERVER=='localhost'){
-	define('DB_SERVER');
-	define('DB_USER','root');
-	define('DB_PWD','spades');
-	define('DB_NAME','inscribe');
-}
+$path = dirname(__FILE__).'/';
+	if(file_exists($path.'../../pranjal'))
+	{
+		define('DB_SERVER','localhost');
+		define('DB_USER','root');
+		define('DB_PWD','regiate');
+		define('DB_NAME','inscribe');
+	}
+	else if(file_exists($path.'../../naman'))
+	{	
+		define('DB_SERVER','localhost');
+		define('DB_USER','root');
+		define('DB_PWD','spades');
+		define('DB_NAME','inscribe');
+	}
+	else
+	{ 	//default values
+		define('DB_SERVER','localhost');
+		define('DB_USER','root');
+		define('DB_PWD','');
+		define('DB_NAME','inscribe');
+	}}
 
 ?>
